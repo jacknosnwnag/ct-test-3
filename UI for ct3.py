5cfde3e3ea4b32f2bcca6e43f9bd421c5b7f08d7
jacknosnwnag <jackson.wang4@education.nsw.gov.au>
2024-08-17 22:00:10 +1000

    Update UI for ct3.py

--- UI for ct3.py
+++ UI for ct3.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 quit = False
 
 #----Setup dataframe and query it here prior to creating visualisation and UI functions----#
-original_df = pd.read_csv('Github/ct-test-3/Data/country_wise_latest.csv') 
-
-Covid_df = pd.read_csv('Github/ct-test-3/Data/country_wise_latest.csv',)
+Covid_df = pd.read_csv('ct-test-3\Data\country_wise_latest.csv',)
 header=None,
 names=['Country', 'Confirmed cases', 'Deaths', 'Recovered','Deaths / 100 cases', 'Recovered / 100 Cases']
 print(Covid_df)
