4fb41dcf75e906a1d199daaed3b8daca99534e8f
jacknosnwnag <jackson.wang4@education.nsw.gov.au>
2024-08-18 08:51:19 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#----Modules----#
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#----Global Variables----#
+quit = False
+
+#----Setup dataframe and query it here prior to creating visualisation and UI functions----#
+Covid_df = pd.read_csv('data\country_wise_latest.csv')
+header=None,
+names=['Country', 'Confirmed cases', 'Deaths', 'Recovered','Deaths / 100 cases', 'Recovered / 100 Cases']
+print(Covid_df)
