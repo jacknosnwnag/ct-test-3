912a98b2dcf1e79c3ebb1539936f36b97833c2ed
jacknosnwnag <jackson.wang4@education.nsw.gov.au>
2024-08-18 09:56:06 +1000

    cleaned dataset

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ quit = False
 Covid_df = pd.read_csv('data\country_wise_latest.csv')
 header=None,
 names=['Country', 'Confirmed cases', 'Deaths', 'Recovered','Deaths / 100 cases', 'Recovered / 100 Cases']
+Covid_df = Covid_df.drop(columns=['New deaths', 'New cases','New recovered','Deaths / 100 Recovered', 'Confirmed last week','1 week change','1 week % increase' ])
+     
 print(Covid_df)
