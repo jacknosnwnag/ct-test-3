53ae69e88d8ad6a0377fe8f0fd03f9b25e425ac4
jacknosnwnag <jackson.wang4@education.nsw.gov.au>
2024-08-18 14:42:18 +1000

    tinkering around with the covid clean thing

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ quit = False
 Covid_df = pd.read_csv('data\country_wise_latest.csv')
 header=None,
 names=['Country', 'Confirmed cases', 'Deaths', 'Recovered','Deaths / 100 cases', 'Recovered / 100 Cases']
-Covid_df = Covid_df.drop(columns=['New deaths', 'New cases','New recovered','Deaths / 100 Recovered', 'Confirmed last week','1 week change','1 week % increase' ])
+Covidclean_df = Covid_df.drop(columns=['New deaths', 'New cases','New recovered','Deaths / 100 Recovered', 'Confirmed last week','1 week change','1 week % increase' ])
      
-print(Covid_df)
+print(Covidclean_df)
+
+def showOriginalData():
+    print(Covid_df)
+
+def showUpdatedData():
+    print(Covidclean_df)
+
+def showCharts():
+    Covidclean_df.plot(
+                    kind='bar',
+                    x='Country',
+                    y='AUD',
+                    color='blue',
+                    alpha=0.3,
+                    title='Cost of a Big Mac in AUD')
+    plt.show()
